dd69ed459243e698f7952865d00359b14d088438
Tirth21896 <Tirth670@gmail.com>
2017-10-14 15:44:39 -0400

    Mario Killer Android Game Saved

---
+++ app/src/main/java/assignment/krpatel/com/androidgame/MainActivity.java
@@ -0,0 +1,25 @@
+package assignment.krpatel.com.androidgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Set No Title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+        this.setContentView(new GameSurface(this));
+    }
+
+}
